11586f047cd11428dba26f7f99a49926a511ccce
Timur-Kotelnikov <crowly086@gmail.com>
2022-09-23 14:29:44 +0300

    added hw6

---
+++ C#_hw/hw_6/hw_6_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Ввод числовых данных с клавиатуры и подсчет количества введенных положительных чисел
+int PositiveCount(string SomeLine)
+{
+    Console.WriteLine(SomeLine);
+    int countPositive = 0;
+    while (true)
+    {
+        string inputLine = Console.ReadLine() ?? "0";
+        bool isNumber = int.TryParse(inputLine, out int inputNumber);
+        if (isNumber)
+        {
+            if (inputNumber > 0)
+            {
+                countPositive += 1;
+            } 
+        }
+        else
+        {
+            break;
+        }
+    }
+    return countPositive;
+}
+
+// Вывод результата
+void PrintResult(int SomeNumber)
+{
+    Console.WriteLine($"Количество введенных положительных чисел: {SomeNumber}");
+}
+
+PrintResult(PositiveCount(("Вводите числа по одному, нажимая Enter. Для завершения ввода введите нечисловое значение и нажмите Enter.")));
---
+++ C#_hw/hw_6/hw_6_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// Ввод данных и задание коэффициентов уравнений
+int ReadData(string SomeLine)
+{
+    Console.WriteLine(SomeLine);
+    int result = int.Parse(Console.ReadLine() ?? "0");
+    return result;
+}
+int k1 = ReadData("k1 =");
+int b1 = ReadData("b1 =");
+int k2 = ReadData("k2 =");
+int b2 = ReadData("b2 =");
+
+// Решение уравнения (определение искомой координаты Х)
+double GetResult(int NumberOne, int NumberTwo, int NumberThree, int NumberFour)
+{
+    double result = (b2 - b1)/(k1 - k2);
+    return result;
+}
+
+// Вывод результата
+void PrintResult(int k1, int b1, int k2, int b2)
+{
+    if (k1 != k2)
+    {
+        Console.WriteLine($"x = {GetResult(k1, b1, k2, b2)}, y = {k1 * (GetResult(k1, b1, k2, b2)) + b1}");
+    }
+    else
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+}
+PrintResult(k1, b1, k2, b2);
